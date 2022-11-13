ae9d2ca4af1f4ad1683944f762510055605bc5a3
ZhuoranT <10852336@qq.com>
2022-11-13 19:37:24 +0800

    test

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+print("test")
 
-# 建立application对象# Python学习群 872937351
-app = QApplication(sys.argv)
-# 建立窗体对象
-w = QWidget()
-# 设置窗体大小
-w.resize(500, 500)
-
-# 设置样式
-w.layout = QVBoxLayout()
-w.label = QLabel("Hello World!")
-w.label.setStyleSheet("font-size:25px;margin-left:155px;")
-w.setWindowTitle("PyQt5 窗口")
-w.layout.addWidget(w.label)
-w.setLayout(w.layout)
-
-# 显示窗体
-w.show()
-# 运行程序
-sys.exit(app.exec_())
+a = 1
